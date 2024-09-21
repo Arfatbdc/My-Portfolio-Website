dd30ee6f1e665f9134bb0414a51e25e9c3c2e4ff
Arfatbdc <137021960+Arfatbdc@users.noreply.github.com>
2024-09-21 06:24:33 +0300

    updated my all folder

---
+++ script.js
@@ -0,0 +1,68 @@
+/* ================= toggle icon navbar =================== */
+let menuIcon = document.querySelector('#menu-icons');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
+
+
+/* ================= Scroll Section Active Link =================== */
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });
+    };
+  });
+
+  /* ================= Sticky Navbar =================== */
+
+  let header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 100);
+
+  /* ================= Remove Toggle icon and navbar when click navbar link (scroll) =================== */
+  menuIcon.classList.remove('bx-x');
+  navbar.classList.remove('active');
+
+};
+
+/* ================= Scroll Reveal =================== */
+ScrollReveal({ 
+ // reset: true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+/* ================= Typed JS =================== */
+const typed = new Typed('.multiple-text', {
+  strings: ['Developer', 'Designer', 'Freelancer'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
+
+
+
+
+
