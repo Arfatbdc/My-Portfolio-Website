bbfc4778e0f65aacebdcb4adf4bb3d883e7ddfdf
Arfatbdc <137021960+Arfatbdc@users.noreply.github.com>
2024-09-24 14:27:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 /* ================= toggle icon navbar =================== */
+/**
+ * Toggles the 'bx-x' class on the menu icon and the 'active' class on the navbar
+ * when the menu icon is clicked.
+ */
 let menuIcon = document.querySelector('#menu-icons');
 let navbar = document.querySelector('.navbar');
 
@@ -7,9 +11,12 @@ menuIcon.onclick = () => {
   navbar.classList.toggle('active');
 }
 
-
-
 /* ================= Scroll Section Active Link =================== */
+/**
+ * Updates the active link in the navbar based on the current scroll position.
+ * Also makes the header sticky when scrolled past a certain point and removes
+ * the toggle icon and navbar active state when a navbar link is clicked.
+ */
 let sections = document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('header nav a');
 
@@ -29,19 +36,28 @@ window.onscroll = () => {
   });
 
   /* ================= Sticky Navbar =================== */
-
+  /**
+   * Toggles the 'sticky' class on the header based on the scroll position.
+   */
   let header = document.querySelector('header');
   header.classList.toggle('sticky', window.scrollY > 100);
 
   /* ================= Remove Toggle icon and navbar when click navbar link (scroll) =================== */
+  /**
+   * Removes the 'bx-x' class from the menu icon and the 'active' class from the navbar
+   * when a navbar link is clicked.
+   */
   menuIcon.classList.remove('bx-x');
   navbar.classList.remove('active');
-
 };
 
 /* ================= Scroll Reveal =================== */
+/**
+ * Initializes [ScrollReveal](https://scrollrevealjs.org/) animations with specified settings and applies
+ * different reveal origins to various elements.
+ */
 ScrollReveal({ 
- // reset: true,
+  reset: true,
   distance: '80px',
   duration: 2000,
   delay: 200
@@ -52,17 +68,15 @@ ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact 
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
-
 /* ================= Typed JS =================== */
+/**
+ * Initializes the [Typed.js](https://www.npmjs.com/package/typed.js) library to create a typing animation on the specified element
+ * with the given strings and typing settings.
+ */
 const typed = new Typed('.multiple-text', {
-  strings: ['Developer', 'Designer', 'Freelancer'],
+  strings: ['Frontend Developer', 'Backend Developer', 'Fullstack Developer'],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
   loop: true
 });
-
-
-
-
-
